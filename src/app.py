0247ae9a6406635ce0ae50bf4fd7558af9d31d8b
Aaron Bostrom <aaron.bostrom1@gmail.com>
2020-06-18 16:29:54 +0100

    big up date to the backend. Now can retrive spells by name, by class and all. This is then matched at school / class and then by level. Can query deep or shallow

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 #!flask/bin/python
 from flask import Flask, jsonify
-from spell import Spell, SpellDataBase, ClassSpellList
+from spell import SpellDataBase
 
 app = Flask(__name__)
 
@@ -12,14 +12,24 @@ def index():
     return "Hello, World!"
 
 
-@app.route('/api/spellcasting', methods=['GET'])
-def get_spells():
-    return jsonify([vars(spell) for spell in spell_db.get_spells()])
-
-@app.route('/api/spellcasting/<string:character_class>', methods=['GET'], defaults={'spell_level': None})
-@app.route('/api/spellcasting/<string:character_class>/<int:spell_level>', methods=['GET'])
+@app.route('/api/spellcasting/spells_by_class/', methods=['GET'], defaults={'character_class':None, 'spell_level': None})
+@app.route('/api/spellcasting/spells_by_class/<string:character_class>', methods=['GET'], defaults={'spell_level': None})
+@app.route('/api/spellcasting/spells_by_class/<string:character_class>/<int:spell_level>', methods=['GET'])
 def get_class_spells(character_class, spell_level):
-    return jsonify([vars(spell) for spell in spell_db.get_spell_list_by_class_and_level(character_class.lower(), spell_level)])
+    return jsonify(spell_db.get_spell_list_by_class_and_level(character_class, spell_level))
+
+
+@app.route('/api/spellcasting/spells_by_school/', methods=['GET'], defaults={'spell_school':None,'spell_level': None})
+@app.route('/api/spellcasting/spells_by_school/<string:spell_school>', methods=['GET'], defaults={'spell_level': None})
+@app.route('/api/spellcasting/spells_by_school/<string:spell_school>/<int:spell_level>', methods=['GET'])
+def get_school_spells(spell_school, spell_level):
+    return jsonify(spell_db.get_spell_list_by_school_and_level(spell_school, spell_level))
+
+
+@app.route('/api/spellcasting/spells_by_name/', methods=['GET'], defaults={'spell_level': None})
+@app.route('/api/spellcasting/spells_by_name/<int:spell_level>', methods=['GET'])
+def get_name_spells(spell_level):
+    return jsonify(spell_db.get_spell_list_by_level(spell_level))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/spell.py
+++ src/spell.py
@@ -1,19 +1,30 @@
-
+from flask import jsonify
+from enum import auto
 import enum
-class SpellComponent(str, enum.Enum):
-    V = "V"
-    S = "S"
-    M = "M"
+class SpellComponent(enum.IntEnum):
+    v = auto()
+    s = auto()
+    m = auto()
 
-class SpellSchools(str, enum.Enum):
-    Abjuration = "Abjuration"
-    Conjuration = "Conjuration"
-    Divination = "Divination"
-    Enchantment = "Enchantment"
-    Evocation = "Evocation"
-    Illusion = "Illusion"
-    Necromancy = "Necromancy"
-    Transmutation = "Transmutation"
+class SpellSchools(enum.IntEnum):
+    abjuration = auto()
+    conjuration =auto()
+    divination = auto()
+    enchantment = auto()
+    evocation = auto()
+    illusion = auto()
+    necromancy = auto()
+    transmutation = auto()
+
+class CharacterClasses(enum.IntEnum):
+    bard = auto()
+    cleric = auto()
+    druid = auto()
+    paladin = auto()
+    ranger = auto()
+    sorcerer = auto()
+    warlock = auto()
+    wizard = auto()
 
 class SpellLevels(enum.IntEnum):
     Cantrip = 0
@@ -34,24 +45,47 @@ class SpellDataBase:
         super().__init__()
 
         self.spells = {
-            1 : Spell(1, "Eldritch Blast", SpellSchools.Evocation, SpellLevels.Cantrip, "1 action", 120, "Instantaneous", [SpellComponent.V, SpellComponent.S], 
-            "A beam of crackling energy streaks toward a creature within range. Make a ranged spell attack against the target. On a hit, the target takes 1d10 force damage. \
-            The spell creates more than one beam when you reach higher levels: two beams at 5th level, three beams at 11th level, and four beams at 17th level. \
-            You can direct the beams at the same target or at different ones. Make a separate attack roll for each beam."), 
+            1 : Spell(1, "Eldritch Blast", SpellSchools.evocation, SpellLevels.Cantrip, "1 action", 120, "Instantaneous", [SpellComponent.v, SpellComponent.s] 
+            #,"A beam of crackling energy streaks toward a creature within range. Make a ranged spell attack against the target. On a hit, the target takes 1d10 force damage. \
+            #The spell creates more than one beam when you reach higher levels: two beams at 5th level, three beams at 11th level, and four beams at 17th level. \
+            #You can direct the beams at the same target or at different ones. Make a separate attack roll for each beam."
+            ), 
             
-            2 :  Spell(1, "Mending", SpellSchools.Transmutation, SpellLevels.Cantrip, "1 action", 0, "Instantaneous", [SpellComponent.V, SpellComponent.S, SpellComponent.M], 
-            "This spell repairs a single break or tear in an object you touch, such as a broken chain link, two halves of a broken key, a torn cloak, or a leaking wineskin. As long as the break or tear is no larger than 1 foot in any dimension, you mend it, leaving no trace of the former damage.\
-            This spell can physically repair a magic item or construct, but the spell can't restore magic to such an object."),
+            2 :  Spell(2, "Mending", SpellSchools.transmutation, SpellLevels.Cantrip, "1 action", 0, "Instantaneous", [SpellComponent.v, SpellComponent.s, SpellComponent.m]
+            ,"This spell repairs a single break or tear in an object you touch, such as a broken chain link, two halves of a broken key, a torn cloak, or a leaking wineskin. As long as the break or tear is no larger than 1 foot in any dimension, you mend it, leaving no trace of the former damage.\
+            This spell can physically repair a magic item or construct, but the spell can't restore magic to such an object."
+            ),
+
+            3 : Spell(3, "Eldritch Smash", SpellSchools.evocation, SpellLevels.level1, "1 action", 120, "Instantaneous", [SpellComponent.v, SpellComponent.s]
+            #,"A beam of crackling energy streaks toward a creature within range. Make a ranged spell attack against the target. On a hit, the target takes 1d10 force damage. \
+            #The spell creates more than one beam when you reach higher levels: two beams at 5th level, three beams at 11th level, and four beams at 17th level. \
+            #You can direct the beams at the same target or at different ones. Make a separate attack roll for each beam."
+            ), 
         }
 
+        #create dictionary of empty spell school objects.
+        self.school_spells = {school: SpellList(school.name) for school in SpellSchools}
+       
+        #append the spell id, to the appropriate data table to get by school.
+        for spell in self.spells.values():
+            self.school_spells[spell.spell_school].add_spell_by_level(spell.spell_id, spell.spell_level)
 
-        bard_spells = ClassSpellList("bard", cantrips=[2])
-        warlock_spells = ClassSpellList("warlock", cantrips=[1])
-
+        #create the class spell lists by level
+        bard_spells = SpellList(CharacterClasses.bard, cantrips=[2])
+        warlock_spells = SpellList(CharacterClasses.warlock, cantrips=[1])
         self.class_spells = {
-            bard_spells.character_class : bard_spells, 
-            warlock_spells.character_class : warlock_spells
+            CharacterClasses.bard : bard_spells, 
+            CharacterClasses.warlock : warlock_spells
         }
+
+        self.spell_levels = SpellList("all")
+        for spell in self.spells.values():
+            self.spell_levels.add_spell_by_level(spell.spell_id, spell.spell_level)
+        
+
+        #print(self.get_spell_list_by_level())
+        print(self.get_spell_list_by_school_and_level())
+        #print(self.get_spell_list_by_class_and_level())
 
     def get_spells(self):
         return self.spells.values()
@@ -59,41 +93,67 @@ class SpellDataBase:
     def get_spell_by_id(self, spell_id):
         return self.spells[spell_id]
 
-    def get_spell_list_by_class_and_level(self, character_class, spell_level=None):
-        return [self.spells[spell_id] for spell_id in self.class_spells[character_class].get_spells_by_level(spell_level)]
+    def get_spell_list_by_class_and_level(self, character_class=None, spell_level=None):
+        if character_class is None:
+            return {char_class.name: {sp_level.name: [vars(self.spells[spell_id]) for spell_id in spell_ids] for sp_level, spell_ids in spell_lists.get_spells_by_level(spell_level).items()} for char_class, spell_lists in self.class_spells.items()}
+        else : 
+            if isinstance(character_class, str):
+                character_class = CharacterClasses[character_class] # convert string to enum
+            return {character_class.name : {sp_level.name: [vars(self.spells[spell_id]) for spell_id in spell_ids] for sp_level, spell_ids in self.class_spells[character_class].get_spells_by_level(spell_level).items()}}
 
 
+    #this returns a spell list by school. and then by level.
+    def get_spell_list_by_school_and_level(self, school=None, spell_level=None):
+        if school is None:
+            return {spell_school.name: {sp_level.name: [vars(self.spells[spell_id]) for spell_id in spell_ids] for sp_level, spell_ids in spell_lists.get_spells_by_level(spell_level).items()} for spell_school, spell_lists in self.school_spells.items()}
+        else : 
+            if isinstance(school, str):
+                school = SpellSchools[school] #convert string to enum
+            return {school : {sp_level.name: [vars(self.spells[spell_id]) for spell_id in spell_ids] for sp_level, spell_ids in self.school_spells[school].get_spells_by_level(spell_level).items()}}
 
-class ClassSpellList:
 
-    def __init__(self, character_class, cantrips=[], level1=[], level2=[], level3=[], level4=[], level5=[], level6=[], level7=[], level8=[], level9=[]):
+    #tested and correct.
+    def get_spell_list_by_level(self, spell_level=None):
+        return {sp_level.name: [vars(self.spells[spell_id]) for spell_id in spell_ids] for sp_level, spell_ids in self.spell_levels.get_spells_by_level(spell_level).items()}
+
+
+class SpellList:
+
+    def __init__(self, name, cantrips=[], level1=[], level2=[], level3=[], level4=[], level5=[], level6=[], level7=[], level8=[], level9=[]):
         super().__init__()
 
-        self.character_class = character_class
-        self.spells = {
-            0: cantrips,
-            1: level1,
-            2: level2,
-            3: level3,
-            4: level4,
-            5: level5,
-            6: level6,
-            7: level7,
-            8: level8,
-            9: level9
+        #default args on mutable objects in python share across classes. so clone into new array when storing.
+        self.name = name
+        self.spells_by_level = {
+            SpellLevels.Cantrip: list(cantrips),
+            SpellLevels.level1: list(level1),
+            SpellLevels.level2: list(level2),
+            SpellLevels.level3: list(level3),
+            SpellLevels.level4: list(level4),
+            SpellLevels.level5: list(level5),
+            SpellLevels.level6: list(level6),
+            SpellLevels.level7: list(level7),
+            SpellLevels.level8: list(level8),
+            SpellLevels.level9: list(level9)
         }
+
+
+    def add_spell_by_level(self, spell_id, spell_level):
+        self.spells_by_level[spell_level].append(spell_id)
 
     def get_spells_by_level(self, spell_level=None):
         if spell_level is None:
-            return [spell_id for spell_ids in self.spells.values() for spell_id in spell_ids ] #return list of all spell ids
+            return self.spells_by_level
         else :
-            return self.spells[spell_level] # else just return the particular level of spell ids
+            if isinstance(spell_level, int):
+                spell_level = SpellLevels[spell_level]            
+            return {spell_level : self.spells_by_level[spell_level]} # else just return the particular level of spell ids
 
 class Spell:
 
-    def __init__(self, id, name, spell_school, spell_level, casting_time, spell_range, duration, components, description):
+    def __init__(self, spell_id, name, spell_school, spell_level, casting_time, spell_range, duration, components, description=""):
         super().__init__()
-        self.id = id
+        self.spell_id = spell_id
         self.name = name
         self.spell_school = spell_school
         self.spell_level = spell_level
@@ -111,3 +171,7 @@ class Spell:
                 'spell_school': self.spell_school,
                 'casting_time': self.casting_time,
             }
+
+
+
+db = SpellDataBase()
