a973368fe449c2e4472db0334aae7f9b6132a059
Aaron Bostrom <aaron.bostrom1@gmail.com>
2020-06-18 13:28:27 +0100

    added enum support to the spell classes to prevent strange data entry. Support for spell schools and spell levels and spell components

--- src/spell.py
+++ src/spell.py
@@ -1,16 +1,45 @@
 
+import enum
+class SpellComponent(str, enum.Enum):
+    V = "V"
+    S = "S"
+    M = "M"
+
+class SpellSchools(str, enum.Enum):
+    Abjuration = "Abjuration"
+    Conjuration = "Conjuration"
+    Divination = "Divination"
+    Enchantment = "Enchantment"
+    Evocation = "Evocation"
+    Illusion = "Illusion"
+    Necromancy = "Necromancy"
+    Transmutation = "Transmutation"
+
+class SpellLevels(enum.IntEnum):
+    Cantrip = 0
+    level1 = 1
+    level2 = 2
+    level3 = 3
+    level4 = 4
+    level5 = 5
+    level6 = 6
+    level7 = 7
+    level8 = 8
+    level9 = 9
+
+
 class SpellDataBase:
 
     def __init__(self):
         super().__init__()
 
         self.spells = {
-            1 : Spell(1, "Eldritch Blast", "Evocation", 0, "1 action", 120, "Instantaneous", ["V", "S"], 
+            1 : Spell(1, "Eldritch Blast", SpellSchools.Evocation, SpellLevels.Cantrip, "1 action", 120, "Instantaneous", [SpellComponent.V, SpellComponent.S], 
             "A beam of crackling energy streaks toward a creature within range. Make a ranged spell attack against the target. On a hit, the target takes 1d10 force damage. \
             The spell creates more than one beam when you reach higher levels: two beams at 5th level, three beams at 11th level, and four beams at 17th level. \
             You can direct the beams at the same target or at different ones. Make a separate attack roll for each beam."), 
             
-            2 :  Spell(1, "Mending", "Transmuation", 0, "1 action", 0, "Instantaneous", ["V", "S", "M"], 
+            2 :  Spell(1, "Mending", SpellSchools.Transmutation, SpellLevels.Cantrip, "1 action", 0, "Instantaneous", [SpellComponent.V, SpellComponent.S, SpellComponent.M], 
             "This spell repairs a single break or tear in an object you touch, such as a broken chain link, two halves of a broken key, a torn cloak, or a leaking wineskin. As long as the break or tear is no larger than 1 foot in any dimension, you mend it, leaving no trace of the former damage.\
             This spell can physically repair a magic item or construct, but the spell can't restore magic to such an object."),
         }
@@ -59,8 +88,6 @@ class ClassSpellList:
             return [spell_id for spell_ids in self.spells.values() for spell_id in spell_ids ] #return list of all spell ids
         else :
             return self.spells[spell_level] # else just return the particular level of spell ids
-
-
 
 class Spell:
 
